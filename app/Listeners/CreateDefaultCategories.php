d22bbab53d1794ef01b11a24bebf61ac0fcf8e75
kurokeita <kuro.keita94@gmail.com>
2019-09-18 17:50:31 +0700

    update default cats

--- app/Listeners/CreateDefaultCategories.php
+++ app/Listeners/CreateDefaultCategories.php
@@ -30,150 +30,157 @@ class CreateDefaultCategories
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Income',
             'parent_id' => 0
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Expense',
+            'parent_id' => 0
+        ]);
+
+        Category::create([
+            'user_id' => $user_id,
+            'type' => 3,
+            'name' => 'Transfer to another wallet',
             'parent_id' => 0
         ]);
         
         $income_id = Category::where('user_id', $user_id)
-                             ->where('type', 'income')
+                             ->where('type', 1)
                              ->first()
                              ->id;
 
         $outcome_id = Category::where('user_id', $user_id)
-                              ->where('type', 'outcome')
+                              ->where('type', 2)
                               ->first()
                               ->id;
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Education',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Entertainment',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Family',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Food & Beverage',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Friends & Lovers',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Health & Fitness',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Investment',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Shopping',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Transportation',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Travel',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Others',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'outcome',
+            'type' => 2,
             'name' => 'Bills & Utilities',
             'parent_id' => $outcome_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Award',
             'parent_id' => $income_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Gifts',
             'parent_id' => $income_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Interest Money',
             'parent_id' => $income_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Salary',
             'parent_id' => $income_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Selling',
             'parent_id' => $income_id
         ]);
 
         Category::create([
             'user_id' => $user_id,
-            'type' => 'income',
+            'type' => 1,
             'name' => 'Others',
             'parent_id' => $income_id
         ]);
