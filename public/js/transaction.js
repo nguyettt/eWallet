2f840dbffc9383e49126ee14ef4dc9b224a933b4
kurokeita <kuro.keita94@gmail.com>
2019-09-18 17:44:28 +0700

    add js script for transaction module

---
+++ public/js/transaction.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    type_select();
+
+    $("#type").change(function () {
+        type_select();
+    });
+})
+
+function type_select () {
+    type = $("#type").val();
+    switch (type) {
+        case '1': {
+            $(".1").css("display", "block");
+            $(".2").css("display", "none");
+            $(".3").css("display", "none");
+            $(".benefit_wallet_block").css("display", "none");
+            $("#cat_id").val("");
+            break;
+        }
+        case '2': {
+            $(".1").css("display", "none");
+            $(".2").css("display", "block");
+            $(".3").css("display", "none");
+            $(".benefit_wallet_block").css("display", "none");
+            $("#cat_id").val("");
+            break;
+        }
+        case '3': {
+            $(".1").css("display", "none");
+            $(".2").css("display", "none");
+            $(".3").css("display", "block");
+            $(".benefit_wallet_block").css("display", "flex");
+            $("#cat_id").val("");
+            break;
+        }
+        default: {
+            $(".1").css("display", "none");
+            $(".2").css("display", "none");
+            $(".3").css("display", "none");
+            $(".benefit_wallet_block").css("display", "none");
+            $("#cat_id").val("");
+            break;
+        }
+    }
+}
