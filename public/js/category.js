4196d5cad06b7af1cef84fae305e452d77fc8314
kurokeita <kuro.keita94@gmail.com>
2019-09-18 17:44:53 +0700

    category js using switch case instead of if else

--- public/js/category.js
+++ public/js/category.js
@@ -1,12 +1,32 @@
 $(document).ready(function () {
     $("#type").change(function () {
-        type = $("#type").val();
-        if (type == "outcome") {
-            $(".income").css("display", "none");
-            $(".outcome").css("display", "block");
-        } else {
-            $(".outcome").css("display", "none");
-            $(".income").css("display", "block");
-        }
+        type_select();
     })
 })
+
+function type_select () {
+    type = $("#type").val();
+    switch (type) {
+        case '1': {
+            $(".1").css("display", "block");
+            $(".2").css("display", "none");
+            $(".3").css("display", "none");
+            $("#parent_id").val("");
+            break;
+        }
+        case '2': {
+            $(".1").css("display", "none");
+            $(".2").css("display", "block");
+            $(".3").css("display", "none");
+            $("#parent_id").val("");
+            break;
+        }
+        case '3': {
+            $(".1").css("display", "none");
+            $(".2").css("display", "none");
+            $(".3").css("display", "block");
+            $("#parent_id").val("");
+            break;
+        }
+    }
+}
