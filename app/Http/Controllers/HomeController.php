ecbdc989fa105056f045716c0aaeaa2463c2d456
kurokeita <kuro.keita94@gmail.com>
2019-09-30 17:18:51 +0700

    refactor code for home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Repository\WalletEloquentRepository;
 use App\Repository\CategoryEloquentRepository;
 use App\Repository\TransactionEloquentRepository;
-use Illuminate\Support\Arr;
+use App\Repository\WalletEloquentRepository;
 
 class HomeController extends Controller
 {
@@ -23,8 +21,7 @@ class HomeController extends Controller
         WalletEloquentRepository $walletRepo,
         CategoryEloquentRepository $catRepo,
         TransactionEloquentRepository $transactionRepo
-    )
-    {
+    ) {
         $this->walletRepo = $walletRepo;
         $this->catRepo = $catRepo;
         $this->transactionRepo = $transactionRepo;
@@ -46,12 +43,12 @@ class HomeController extends Controller
         $year = date('Y');
 
         $transaction = $this->transactionRepo->query()
-                                            ->where('delete_flag', null)
-                                            ->where('type', '<>', 3)
-                                            ->whereMonth('created_at', $month)
-                                            ->whereYear('created_at', $year)
-                                            ->get();
-        
+            ->where('delete_flag', null)
+            ->where('type', '<>', 3)
+            ->whereMonth('created_at', $month)
+            ->whereYear('created_at', $year)
+            ->get();
+
         foreach ($transaction as $item) {
             if ($item->type == 1) {
                 $inflow += $item->amount;
@@ -59,28 +56,30 @@ class HomeController extends Controller
                 $outflow += $item->amount;
             }
         }
-        
+
         switch ($month) {
-            case 1: {
-                $before = ($year-1).'-01-01 00:00:00';
-                break;
-            }
-            default: {
-                $before = $year.'-'.$month.'-01 00:00:00';
-                break;
-            }
+            case 1:{
+                    $before = ($year - 1) . '-01-01 00:00:00';
+                    break;
+                }
+            default:{
+                    $before = $year . '-' . $month . '-01 00:00:00';
+                    break;
+                }
         }
-        
+
         $income = $transaction->where('type', 1);
         $income = $income->sortByDesc(function ($item) {
             return $item->amount;
         })->values();
-        
+
         if (count($income) >= 5) {
             for ($i = 0; $i < 5; $i++) {
                 $top_income[] = $income[$i];
             }
-        } else $top_income = $income;
+        } else {
+            $top_income = $income;
+        }
 
         $outcome = $transaction->where('type', 2);
         $outcome = $outcome->sortByDesc(function ($item) {
@@ -91,13 +90,15 @@ class HomeController extends Controller
             for ($i = 0; $i < 5; $i++) {
                 $top_outcome[] = $outcome[$i];
             }
-        } else $top_outcome = $outcome;
+        } else {
+            $top_outcome = $outcome;
+        }
 
         $_transaction = $this->transactionRepo->query()
-                                            ->where('created_at', '<', $before)
-                                            ->where('delete_flag', null)
-                                            ->where('type', '<>', 3)
-                                            ->get();
+            ->where('created_at', '<', $before)
+            ->where('delete_flag', null)
+            ->where('type', '<>', 3)
+            ->get();
         $startingBalance = 0;
 
         foreach ($_transaction as $item) {
@@ -107,7 +108,7 @@ class HomeController extends Controller
                 $startingBalance -= $item->amount;
             }
         }
-        
+
         return view('dashboard.dashboard', compact('balance', 'startingBalance', 'inflow', 'outflow', 'top_income', 'top_outcome'));
     }
 }
