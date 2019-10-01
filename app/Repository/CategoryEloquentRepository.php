683b6a9d85f1ca794884e7cba897487129dac8a0
kurokeita <kuro.keita94@gmail.com>
2019-10-01 12:07:02 +0700

    add get child list function

--- app/Repository/CategoryEloquentRepository.php
+++ app/Repository/CategoryEloquentRepository.php
@@ -21,4 +21,42 @@ class CategoryEloquentRepository extends EloquentRepository {
             'parent_id',
         ];
     }
+
+    /**
+     * Call the _findChild method to build an array of cat_id that belong to the specified id
+     *
+     * @param int $parent
+     * @return array $child
+     */
+    public function findChild($parent)
+    {
+        $list = $this->getAll();
+        return $this->_findChild($parent, $list);
+    }
+
+    /**
+     * Build an array of category id that belong to the $parent id
+     * 
+     * @param int  $id
+     * @param collection $list
+     * @return array $child
+     */
+    public function _findChild ($parent, $list)
+    {
+        $_child = $list->where('parent_id', $parent);
+
+        if ($_child->count() > 0) {
+            foreach ($_child as $item) {
+                $child[] = $item->id;
+                $new = $this->_findChild($item->id, $list);
+                if ($new != null) {
+                    $child = array_merge($child, $new);
+                }
+            }
+        } else {
+            $child = null;
+        }
+
+        return $child;
+    }
 }
